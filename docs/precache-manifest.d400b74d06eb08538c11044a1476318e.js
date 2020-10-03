f0d3c13e45d761066c0a7871955cf0500837d1bb
Eduardo Iglesias <eiglesiasdiaz@outlook.com>
2020-10-02 20:20:22 -0500

    docs agregado

---
+++ docs/precache-manifest.d400b74d06eb08538c11044a1476318e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a128cd1f6c0b2a3c1627353600ac93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "450ac18297bf1cc4e989",
+    "url": "/static/css/main.ae4f0da1.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450ac18297bf1cc4e989",
+    "url": "/static/js/main.2f5ec101.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
